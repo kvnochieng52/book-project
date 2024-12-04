76b55a754944c3231336f8cc9a300ff5c1875ffc
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-12-04 10:23:33 +0300

    APPROVED NOT COLLECTED

--- config/session.php
+++ config/session.php
@@ -31,7 +31,9 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    //'lifetime' => env('SESSION_LIFETIME', 120),
+
+    'lifetime' => 525600,
 
     'expire_on_close' => false,
 
@@ -128,7 +130,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
